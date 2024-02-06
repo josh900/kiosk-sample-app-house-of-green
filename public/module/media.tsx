db5da96799b065f4f41781fc5b0d0ce2ee356051
josh900 <jm5522@sbcglobal.net>
2024-02-06 15:55:57 -0800

    Revert "update to embed"
    This reverts commit 36654a190a523324e636c9826d8212d3fedd26a0.

--- public/module/media.tsx
+++ public/module/media.tsx
@@ -24,41 +24,43 @@ const Media = ({ mediaUrl }: { mediaUrl: string }) => {
         position: "relative"        
       }}        
     >        
- <embed 
-    className="firstframe"  
-    src={`https://aws-amplify-signage-player.vercel.app/?playlist_id=${playlistId}&backend_url=https://api-v2.skoopsignage.app`}        
-    width="100%"        
-    height="100%"        
-    title="Web Player"        
-    style={{   
-      opacity: displayMediaUrl ? 0 : 1,   
-      visibility: displayMediaUrl ? 'hidden' : 'visible',   
-      border: 'none',   
-      overflowY: 'scroll',
-      overflowX: 'hidden',
-      transition: 'opacity 0.25s',  
-      position: 'absolute',   
-      top: 0,   
-      left: 0   
-    }}  
-/>        
-<embed        
-    src="https://thehouseofgreen.wm.store/"        
-    width="100%"        
-    height="100%"        
-    title="Google"        
-    style={{   
-      opacity: displayMediaUrl ? 1 : 0,   
-      visibility: displayMediaUrl ? 'visible' : 'hidden',   
-      border: 'none',   
-      overflowY: 'scroll',
-      overflowX: 'hidden',
-      transition: 'opacity 0.25s',  
-      position: 'absolute',   
-      top: 0,   
-      left: 0   
-    }}        
-/>        
+      <iframe        
+        className="firstframe"  
+        src={`https://aws-amplify-signage-player.vercel.app/?playlist_id=${playlistId}&backend_url=https://api-v2.skoopsignage.app`}        
+        width="100%"        
+        height="100%"        
+        title="Web Player"        
+        style={{   
+          opacity: displayMediaUrl ? 0 : 1,   
+          visibility: displayMediaUrl ? 'hidden' : 'visible',   
+          border: 'none',   
+          overflowY: 'scroll',
+          overflowX: 'hidden',
+          transition: 'opacity 0.25s',  
+          position: 'absolute',   
+          top: 0,   
+          left: 0   
+        }}  
+        scrolling="yes"       
+      />        
+      <iframe        
+        src="https://thehouseofgreen.wm.store/"        
+        width="100%"        
+        height="100%"        
+        title="Google"        
+        style={{   
+          opacity: displayMediaUrl ? 1 : 0,   
+          visibility: displayMediaUrl ? 'visible' : 'hidden',   
+          border: 'none',   
+          overflowY: 'scroll',
+          overflowX: 'hidden',
+          transition: 'opacity 0.25s',  
+          position: 'absolute',   
+          top: 0,   
+          left: 0   
+        }}        
+        scrolling="yes" 
+      />        
       <Box        
         sx={{        
           position: "absolute",        
