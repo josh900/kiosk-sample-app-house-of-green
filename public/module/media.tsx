fa71ac7819cab3ea23bf24d5d72029269f589089
josh900 <jm5522@sbcglobal.net>
2024-02-06 17:15:38 -0800

    Update media.tsx

--- public/module/media.tsx
+++ public/module/media.tsx
@@ -28,7 +28,7 @@ const Media = ({ mediaUrl }: { mediaUrl: string }) => {
         className="firstframe"  
         src={`https://aws-amplify-signage-player.vercel.app/?playlist_id=${playlistId}&backend_url=https://api-v2.skoopsignage.app`}        
         width="100%"        
-        height="99999px"        
+        height="100%"        
         title="Web Player"        
         style={{   
           opacity: displayMediaUrl ? 0 : 1,   
